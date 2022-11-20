e5e81600e9a98c461e1531d5d431d5f9a69e0e51
jeanfernandespro <jean.fernandes@protalentsolutions.com.br>
2022-11-20 08:01:42 -0300

    update usersController e tasksController

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,5 +1,3 @@
-const jsonwebtoken = require('jsonwebtoken');
-const authorizationToken = require('../middlewares/authorizationToken');
 const tasksModel = require('../models/tasksModel');
 
 const getAll = async (_request, response) => {
@@ -7,52 +5,14 @@ const getAll = async (_request, response) => {
   return response.status(200).json(tasks);
 };
 
-const getAllUsers = async (_request, response) => {
-  const users = await tasksModel.getAllUsers();
-  return response.status(200).json(users);
-};
-
 const getById = async (request, response) => {
   const tasks = await tasksModel.getById(request.params.id);
   return response.status(200).json(tasks);
 };
 
-const getByIdUser = async (request, response) => {
-  const users = await tasksModel.getByIdUser(request.params.id);
-  return response.status(200).json(users);
-};
-
-const getByLogin = async (request, response) => {
-  try {
-    const user = await tasksModel.getByLogin(
-      request.body.email,
-      request.body.password
-    );
-    const correctPassword = user && user.length > 0;
-    if (!correctPassword)
-      return response.status(401).send('Password or E-mail incorrect');
-    const token = jsonwebtoken.sign(
-      { user: JSON.stringify(user[0]) },
-      authorizationToken.PRIVATE_KEY,
-      {
-        expiresIn: '1d',
-      }
-    );
-    return response.status(200).json({ data: { user: user, token } });
-  } catch (error) {
-    console.log(error);
-    return response.send(error);
-  }
-};
-
 const createTask = async (request, response) => {
   const createdTask = await tasksModel.createTask(request.body);
   return response.status(201).json(createdTask);
-};
-
-const createUser = async (request, response) => {
-  const createdUser = await tasksModel.createUser(request.body);
-  return response.status(201).json(createdUser);
 };
 
 const deleteTask = async (request, response) => {
@@ -69,12 +29,8 @@ const updateTask = async (request, response) => {
 
 module.exports = {
   getAll,
-  getAllUsers,
   getById,
-  getByIdUser,
-  getByLogin,
   createTask,
-  createUser,
   deleteTask,
   updateTask,
 };
---
+++ src/controllers/usersController.js
@@ -0,0 +1,48 @@
+const jsonwebtoken = require('jsonwebtoken');
+const authorizationToken = require('../middlewares/authorizationToken');
+const usersModel = require('../models/usersModel');
+
+const getAllUsers = async (_request, response) => {
+  const users = await usersModel.getAllUsers();
+  return response.status(200).json(users);
+};
+
+const getByIdUser = async (request, response) => {
+  const users = await usersModel.getByIdUser(request.params.id);
+  return response.status(200).json(users);
+};
+
+const getByLogin = async (request, response) => {
+  try {
+    const user = await usersModel.getByLogin(
+      request.body.email,
+      request.body.password
+    );
+    const correctPassword = user && user.length > 0;
+    if (!correctPassword)
+      return response.status(401).send('Password or E-mail incorrect');
+    const token = jsonwebtoken.sign(
+      { user: JSON.stringify(user[0]) },
+      authorizationToken.PRIVATE_KEY,
+      {
+        expiresIn: '1d',
+      }
+    );
+    return response.status(200).json({ data: { user: user, token } });
+  } catch (error) {
+    console.log(error);
+    return response.send(error);
+  }
+};
+
+const createUser = async (request, response) => {
+  const createdUser = await usersModel.createUser(request.body);
+  return response.status(201).json(createdUser);
+};
+
+module.exports = {
+  getAllUsers,
+  getByIdUser,
+  getByLogin,
+  createUser,
+};
