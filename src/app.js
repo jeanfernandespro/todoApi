cf8e9560545c738843995ff112686b0e6434dd3e
jeanfernandespro <jean.fernandes@protalentsolutions.com.br>
2022-11-24 16:09:53 -0300

    comentario app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// Setup básico do app.js | o cors é usado para permitir o uso em localhost (deve ser usado apenas em desenvolvimento)
+// Setup básico do app.js | o cors é usado para permitir o uso em localhost
 const express = require('express');
 const router = require('./router');
 const cors = require('cors');
